f0de86b7b4d6b612f3ddee4738760aa3f942c401
YouEclipse <401556211@qq.com>
2014-11-07 18:45:29 +0800

    有了统计周围雷数的功能。扫雷基本程序已完成60%

---
+++ WinMine/winmine.h
@@ -0,0 +1,33 @@
+//##############################不同等级窗口大小################################
+
+//##############################不同等级雷区域的大小***************************
+
+
+
+#define DEFAULT_ROW_NUM  10
+#define DEFAULT_COL_NUM  10
+#define DEFAULT_MINE_NUM  10
+
+#define PRIMARY_ROW_NUM  10
+#define PRIMARY_COL_NUM  10
+#define PRIMARY_MINE_NUM  10
+
+#define INTERMEDIATE_ROW_NUM  15
+#define INTERMEDIATE_COL_NUM  15
+#define INTERMEDIATE_MINE_NUM   25
+
+#define ADVANCE_ROW_NUM  20
+#define ADVANCE_COL_NUM  20
+#define ADVANCE_MINE_NUM  50
+
+#define MAX_ROW_NUM  20
+#define MAX_COL_NUM  20
+#define MAX_MINE_NUM  50
+
+
+//##############################################################################
+#define NOBOMB 0x100
+#define BOMB 0x101
+
+
+
